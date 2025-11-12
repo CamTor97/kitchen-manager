be8f38911fc9544ccdca56efcb927767035889e2
CamTor97 <cami.tor9710@gmail.com>
2025-11-12 10:30:26 -0500

    Ingredient views creation

--- restaurant_project/kitchenmanager/views.py
+++ restaurant_project/kitchenmanager/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
-
+from django.views.generic import (ListView, DeleteView, DetailView, CreateView, UpdateView)
+from django.urls import reverse_lazy
+from .models import Ingredient, MenuItem, RecipeRequirements, Purchase
 # Create your views here.
+# Ingredient model views
+class IngredientListView(ListView):
+    model = Ingredient
+    template_name = ""
+    context_object_name = "ingredient" 
+
+class IngredientCreationView(CreateView):
+    model = Ingredient
+    fields = ["name", "in_stock", "unit_type", "unit_price"]
+    template_name = ""
+    success_url= reverse_lazy("")
+
+class IngredientUpdateView(UpdateView):
+    model = Ingredient
+    fields = ["name", "in_stock", "unit_type", "unit_price"]
+    template_name = ""
+    success_url= reverse_lazy("")
+
+class IngredientDeleteView(DeleteView):
+    model = Ingredient
+    template_name = ""
+    success_url = reverse_lazy("")
+
